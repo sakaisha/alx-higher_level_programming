64f5569c020d22f16e0bb3f03422a0c6f5e0ab13
Sakaisha <aishasakkary@outlook.com>
2023-11-01 14:55:27 -0700

    fix advanced

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,6 @@
-#!/usr/bin/python3
-for i in range(122, 96, -1):
-    print("{}".format(chr(i - 32) if i % 2 == 0 
-        else chr(i)), end="")
-print()
+for i in reversed(range(97, 123)):
+    if (i % 2 == 0):
+        c = chr(i)
+    else:
+        c = chr(i - 32)
+    print("{}".format(c), end='')
