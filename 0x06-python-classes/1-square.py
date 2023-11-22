2e8c27ad7d0efd121af399bfc6652016df6d0bfb
Sakaisha <aishasakkary@outlook.com>
2023-11-21 17:55:01 -0800

    s

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
 This module defines a Square class.
 
@@ -11,7 +10,7 @@ Example:
 """
 
 class Square:
-        """
+    """
     This class represents a square.
 
     Attributes:
@@ -19,7 +18,8 @@ class Square:
     """
 
     def __init__(self, size):
-         """Initializes the square with the given size.
+        """
+        Initializes a new Square instance.
 
         Args:
             size (int): The size of the square.
