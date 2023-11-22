b2f819a4eab812d2efa7a2d32988ef10dd5a303b
Sakaisha <aishasakkary@outlook.com>
2023-11-21 18:02:33 -0800

    test

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,29 +1,36 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class.
+
+A Square represents a geometric shape with equal sides.
+
+Attributes:
+    size (int): The size of the square's sides.
+
+Methods:
+    __init__(self, size=0): Initializes a new
+    instance of the Square class.
+
+Raises:
+    TypeError: If size is not an integer.
+    ValueError: If size is less than 0.
+"""
+
 class Square:
-    """A class representing a square.
-
-    Attributes:
-        __size (int): The size of the square.
-
-    Methods:
-        __init__(self, size=0): Initializes the Square object with a specified size.
-
-    Raises:
-        TypeError: If the provided size is not an integer.
-        ValueError: If the provided size is less than 0.
-    """
     def __init__(self, size=0):
-        """Initializes the Square object with a specified size.
+        """
+        Initializes a new instance of the Square class.
 
         Args:
-            size (int, optional): The size of the square. Defaults to 0.
+            size (int): The size of the square's sides.
 
         Raises:
-            TypeError: If the provided size is not an integer.
-            ValueError: If the provided size is less than 0.
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
         """
         self.__size = size
         if size != int(size):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
+
