dd4ca7b0022b4548413f83f8e079b21d7463ca4c
Sakaisha <aishasakkary@outlook.com>
2023-11-21 17:21:22 -0800

    first

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 class Square:
-    """Represents a square.""" 
-
-    def __init__(self):
-        """Initializes a new instance of the Square class."""
-        pass
+    """
+    Represents a square.
+    """
+    pass
