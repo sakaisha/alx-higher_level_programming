96ad92fa8d09791df7b9e531462b7b05b0172db8
Sakaisha <aishasakkary@outlook.com>
2023-11-27 15:31:42 -0800

    test

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Class Rectangle Module 2"""
 
+
 class Rectangle:
     """A class representing a rectangle."""
 
--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,36 +1,23 @@
 #!/usr/bin/python3
-"""Class rectangle module 3"""
+"""Class rectangle 4"""
 
 
 class Rectangle:
-    """A class representing a rectangle.
-
-    Attributes:
-        width (int): The width of the rectangle.
-        height (int): The height of the rectangle.
-    """
+    """A class representing a rectangle."""
 
     def __init__(self, width=0, height=0):
-        """Initializes a rectangle with specified width and height."""
+        """Initialize the rectangle with specified width and height."""
         self.height = height
         self.width = width
 
     @property
     def height(self):
-        """int: The height of the rectangle."""
+        """Get the height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Sets the height of the rectangle.
-
-        Args:
-            value (int): The new height value.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than 0.
-        """
+        """Set the height of the rectangle with type and value checks."""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value < 0:
@@ -40,20 +27,12 @@ class Rectangle:
 
     @property
     def width(self):
-        """int: The width of the rectangle."""
+        """Get the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Sets the width of the rectangle.
-
-        Args:
-            value (int): The new width value.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than 0.
-        """
+        """Set the width of the rectangle with type and value checks."""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -62,12 +41,21 @@ class Rectangle:
             self.__width = value
 
     def area(self):
-        """Calculates the area of the rectangle."""
+        """Calculate and return the area of the rectangle."""
         return self.__width * self.__height
 
     def perimeter(self):
-        """Calculates the perimeter of the rectangle."""
+        """Calculate and return the perimeter of the rectangle."""
         if self.__width == 0 or self.__height == 0:
             return 0
         else:
             return (self.__width + self.__height) * 2
+
+    def __str__(self):
+        """Return a string representation of the rectangle."""
+        lis = []
+        for i in range(self.__height):
+            if self.__width > 0:
+                lis += ["#" * self.__width]
+        return "\n".join(lis)
+
