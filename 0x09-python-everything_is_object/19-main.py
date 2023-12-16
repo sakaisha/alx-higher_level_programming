07f106a2a9525e9728fbd2462e841cb71cdc4052
Sakaisha <aishasakkary@outlook.com>
2023-12-15 20:57:38 -0800

    no doubt

--- 0x09-python-everything_is_object/19-main.py
+++ 0x09-python-everything_is_object/19-main.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 copy_list = __import__('19-copy_list').copy_list
 
-Original_List = [1, 2, 3]
-print("Original List:", Original_List)
+my_list = [1, 2, 3]
+print(my_list)
 
-Copied_List = copy_list(Original_List)
+new_list = copy_list(my_list)
 
-print("Original List:", Original_List)
-print("Copied List:", Copied_List)
+print(my_list)
+print(new_list)
 
-print("Are the lists equal?", Copied_List == Original_List)
-print("Are the lists identical?", Copied_List is Original_List)
+print(new_list == my_list)
+print(new_list is my_list)
