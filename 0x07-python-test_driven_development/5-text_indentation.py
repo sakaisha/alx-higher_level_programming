98c021f4181b11cbe189f60fade8516b6cd8ade6
Sakaisha <aishasakkary@outlook.com>
2023-12-15 21:17:15 -0800

    new

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module for text_indentation"""
+
+
+def text_indentation(text):
+    """text_indentation - indent and print text according to cases
+
+    Args:
+    text - text to indent and print
+    """
+    flag = 0
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    textcpy = text.strip(" ")
+    for i in textcpy:
+        if i == '.' or i == '?' or i == ':':
+            print("{}\n\n".format(i), end="")
+            flag = 1
+        elif i == " " and flag == 1:
+                pass
+        else:
+            print("{}".format(i), end="")
+            if i == '\n':
+                flag = 1
+            else:
+                flag = 0
