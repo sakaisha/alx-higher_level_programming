09f2313a56c3a032ad539ab838137857471eb3bd
Sakaisha <aishasakkary@outlook.com>
2023-12-15 21:20:34 -0800

    sup

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,23 +1,32 @@
-#!/usr/bin/python3
-"""Unit tests for the max_integer function.
-
-This module contains a set of unittests for the max_integer function,
-which finds the maximum integer in a given list.
-
-Uses the Google docstring style and adheres to PEP 8 style guidelines.
-
+"""Unittest for max_integer([..])
 """
-
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
-class TestMax(unittest.TestCase):
-    """Test cases for the max_integer function."""
 
-    def test_max(self):
-        """Test max_integer with various input lists."""
-        self.assertEqual(max_integer([1, 2, 3, 4]), 4)  # pycodestyle: E241
-        self.assertEqual(max_integer([1, 2, 4, 3]), 4)  # pycodestyle: E241
+class TestMaxInteger(unittest.TestCase):
+    """Class for testing max integer of a list"""
+    def test_max_integer(self):
+        """Test max integer of a list"""
+        """
+        SUCCESS
+        """
+        self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
+        self.assertEqual(max_integer([1, 88, 3, -6, -3]), 88)
+        self.assertEqual(max_integer([-41, -32, -6, -4, -1]), -1)
+        self.assertEqual(max_integer([99, 6, 43]), 99)
+        self.assertEqual(max_integer([44, 52, 13, 79, 12, 1098, 31]), 1098)
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer(["cats", "hi"]), "hi")
+        self.assertEqual(max_integer([5]), 5)
 
-if __name__ == '__main__':
+        """
+        ERROR
+        """
+        with self.assertRaises(TypeError):
+            max_integer(4)
+        with self.assertRaises(TypeError):
+            max_integer(["cats", 7])
+
+if __name__ == "__main__":
     unittest.main()
