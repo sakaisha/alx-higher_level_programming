ecf14970378e43550add59855128ead0de215599
sakaisha <aishasakkary@outlook.com>
2024-02-11 01:22:17 -0800

    remove the unwanted

--- 0x0A-python-inheritance/0-lookup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Module to perform object lookup."""
-
-
-def lookup(obj):
-    """Returns a list of attributes and methods of the input object."""
-    return dir(obj)
--- 0x0A-python-inheritance/5-base_geometry.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""BaseGeometry module"""
-
-
-class BaseGeometry:
-    """Create a new class representing basic geometry."""
-    pass
