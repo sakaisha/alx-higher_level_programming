97e9c47239c4c24a8fb43a35bf7f4cda5bfdbbc6
sakaisha <aishasakkary@outlook.com>
2024-02-12 16:41:01 -0800

    fix

--- 0x08-python-more_classes/0-rectangle.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""rectangle class module"""
-
-
-class Rectangle:
-    """rectangle class"""
-    pass
--- 0x09-python-everything_is_object/100-magic_string.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def magic_string(m_string=[]):
-    m_string.append("BestSchool")
-    return (", ".join(m_string))
